1720e402c0e5c067afd091f57a943b65064e69ec
vitorlima <vitor_lp@yahoo.com>
2015-06-22 12:19:18 -0300

    Made (morphological) erosion

--- morphological_gradient.m
+++ morphological_gradient.m
@@ -10,6 +10,24 @@
 %###########################################
 
 
+function print_flat_structuring_element(b)
+	N = size(b)(1)
+	if 0 == N
+		printf("{ }\n")
+	else
+		printf "{"
+		for k = 1:size(b)(1)-1
+			x = b(k, 1);
+			y = b(k, 2);
+			printf("(%d , %d), ", x, y) 
+		end
+		x = b(N, 1);
+		y = b(N, 2);
+		printf("(%d , %d)}\n", x, y) 
+	end
+endfunction
+
+
 %   This function checks if the intersection between img
 % and the structuring element b centered in (i, j) is non-empty.
 %	Note: b must be represented as a set of pairs, for instance
@@ -58,18 +76,71 @@ function dlt = dilation(img, b)
 endfunction
 
 
-A = [0 0 0 0 0 0;
+
+%   This function centers the structuring element b in (i, j)
+% and looks to its intersection with the image img, returning 
+% the minimum value of this intersection.
+%	Note: b must be represented as a set of pairs, for instance
+%			b = [ [1, 1]; [0, 0]; [-1, -1]]
+%						    1 0 0		
+%					==> b = 0 X 0    (where X, position (0, 0),  is the origin)
+%							0 0 1
+%		or
+%			b = [ [0, 1]; [0, 0]; [0, -1]]
+%						    0 0 0		
+%					==> b = 1 X 1    (where X, position (0, 0), is the origin)
+%							0 0 0
+function value = minimum_value_inside_intersection(img, b, i, j)
+	[N M] = size(img);
+	value = 256;
+	for k = 1:size(b)(1) 
+		% get (x, y) coordinates from b
+		x = b(k, 1);
+		y = b(k, 2);
+		% if it's inside the image boundaries
+		if 0 < i + x && i + x <= N \
+		  && 0 < j + y && j + y <= M \
+		  % if has intersection and it's greter then the previous intersection 
+		  && img(i + x, j + y) < value
+		  	value = img(i + x, j + y);
+		end
+	end
+
+endfunction
+
+
+%   Receives a matrix representing a grayscale image and 
+% another matrix representing the structuring element.
+%	Find its (morphological) erosion, returning it.
+function ers = erosion(img, b)
+	L = 256;
+	[N M] = size(img);
+	ers = zeros(N, M);
+
+	for i=1:N
+		for j=1:M
+			min_val = minimum_value_inside_intersection(img, b, i,j);
+			ers(i, j) = min_val;
+		end
+	end
+endfunction
+
+
+A = [0 0 3 0 0 0;
      0 1 2 3 0 0;
 	 0 4 5 1 3 0;
-	 0 0 1 0 0 0;
+	 0 9 1 0 0 0;
 	 0 0 0 0 0 0]
 
 b = [-1 0;
 	 0 0;
-	 1 0]
+	 1 0];
+
+print_flat_structuring_element(b)
 
 dlt = dilation(A, b)
 
+ers = erosion(A, b)
 
 
 %file = input('Type the path of the image: ', 's');
